8c87caf40353ec6f08dbda9313e7ccf6cbf6ad14
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-09 15:55:50 +0530

    refractor finding the reverse of given number and adding file to logical repo

---
+++ src/com/logical/reversenumber/ReverseNumber.java
@@ -0,0 +1,26 @@
+package com.logical.reversenumber;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter a number : ");
+		int num = scan.nextInt();
+
+		int revnum = 0;
+		int rem = 0;
+
+		while (num > 0) {
+			rem = num % 10;
+			revnum = revnum * 10 + rem;
+			num = num / 10;
+		}
+		System.out.println("Reverse of the given number : " + revnum);
+		scan.close();
+	}
+
+}
